c5df0ba5c4f9edd5ccdec0c5a3e443e740e67c84
Kenneth Kan <kenhkan@gmail.com>
2013-12-14 16:17:35 -0500

    fix(loader): check for LazyLoad instead of capturing error

--- app/assets/init.js
+++ app/assets/init.js
@@ -3,6 +3,12 @@
 // Initialize our application
 (function() {
   function init() {
+    // If LazyLoad isn't available yet, check again later
+    if (LazyLoad === void 0) {
+      setTimeout(init, 0);
+      return;
+    }
+
     var APP_NAME = window.__APP_NAME;
     var BASE_URL = window.__BASE_URL;
     var BASE_ELEMENT = window.__BASE_ELEMENT;
@@ -11,18 +17,13 @@
     // Prepare application controller
     BASE_ELEMENT.setAttribute('ng-controller', APP_CONTROLLER_NAME);
 
-    try {
-      // Load the libraries, templates, configuration, then the app itself
-      LazyLoad.js(['vendor.js', 'templates.js', 'config.js', 'app.js'], function() {
-        angular.bootstrap(BASE_ELEMENT, [APP_NAME]);
+    // Load the libraries, templates, configuration, then the app itself
+    LazyLoad.js(['vendor.js', 'templates.js', 'config.js', 'app.js'], function() {
+      angular.bootstrap(BASE_ELEMENT, [APP_NAME]);
 
-        // We have loaded
-        window.__APP_LOADED = true;
-      });
-    } catch (error) {
-      console.log(error);
-      setTimeout(init, 0);
-    }
+      // We have loaded
+      window.__APP_LOADED = true;
+    });
   }
 
   // Start initialization
