a931d3dc4a9a92c4e64c48d195eb6d7d0f817a78
Kenneth Kan <kenhkan@gmail.com>
2013-12-14 16:14:39 -0500

    feat(loader): restart initialization on failure

--- app/assets/init.js
+++ app/assets/init.js
@@ -2,19 +2,29 @@
 
 // Initialize our application
 (function() {
-  var APP_NAME = window.__APP_NAME;
-  var BASE_URL = window.__BASE_URL;
-  var BASE_ELEMENT = window.__BASE_ELEMENT;
-  var APP_CONTROLLER_NAME = window.__APP_CONTROLLER_NAME;
+  function init() {
+    var APP_NAME = window.__APP_NAME;
+    var BASE_URL = window.__BASE_URL;
+    var BASE_ELEMENT = window.__BASE_ELEMENT;
+    var APP_CONTROLLER_NAME = window.__APP_CONTROLLER_NAME;
 
-  // Prepare application controller
-  BASE_ELEMENT.setAttribute('ng-controller', APP_CONTROLLER_NAME);
+    // Prepare application controller
+    BASE_ELEMENT.setAttribute('ng-controller', APP_CONTROLLER_NAME);
 
-  // Load the libraries, templates, configuration, then the app itself
-  LazyLoad.js(['vendor.js', 'templates.js', 'config.js', 'app.js'], function() {
-    angular.bootstrap(BASE_ELEMENT, [APP_NAME]);
+    try {
+      // Load the libraries, templates, configuration, then the app itself
+      LazyLoad.js(['vendor.js', 'templates.js', 'config.js', 'app.js'], function() {
+        angular.bootstrap(BASE_ELEMENT, [APP_NAME]);
 
-    // We have loaded
-    window.__APP_LOADED = true;
-  });
+        // We have loaded
+        window.__APP_LOADED = true;
+      });
+    } catch (error) {
+      console.log(error);
+      setTimeout(init, 0);
+    }
+  }
+
+  // Start initialization
+  setTimeout(init, 0);
 })();
